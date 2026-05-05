0b77a14cedc370ead87e4b22848adce59d7b08de
Tomas Tomeska <tomas@reservio.online>
2026-05-05 12:03:07 +0200

    Fix admin include chain for coach welcome email

--- includes/admin_auth.php
+++ includes/admin_auth.php
@@ -5,6 +5,7 @@
 
 require_once __DIR__ . '/../config/config.php';
 require_once __DIR__ . '/../config/database.php';
+require_once __DIR__ . '/functions.php';
 
 if (session_status() === PHP_SESSION_NONE) {
     session_name(SESSION_NAME);
